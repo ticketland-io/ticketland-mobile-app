9d91f07df69ad83e5541464f56af08b311506bdf
George Niotis <geoniotis@yahoo.com>
2023-01-26 21:22:46 +0200

    feat: adding hooks

---
+++ src/view/hooks/useWeb3.js
@@ -0,0 +1,27 @@
+import {useEffect, useState, useContext} from 'react'
+import Web3 from '@apocentre/solana-web3'
+import {Context} from '../core/Store'
+
+export default () => {
+  const [web3, setWeb3] = useState(null)
+  const [state, _] = useContext(Context)
+
+  useEffect(() => {
+    const initWeb3 = async () => {
+      if (state.connection && state.user && !state.web3) {
+        const _web3 = Web3()
+
+        if (state.walletType === 'custody') {
+          const custodyWallet = await state.eutopicCore.bootstrap(state.user)
+          await _web3.init(state.connection, custodyWallet)
+        }
+
+        setWeb3(_web3)
+      }
+    }
+
+    initWeb3().catch(error => console.error('Failed to initialize web3: ', error))
+  }, [state.connection, state.user])
+
+  return web3
+}

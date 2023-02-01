4cda70c5cf3811fefc69364a261905496f1fc655
George Niotis <geoniotis@yahoo.com>
2023-02-01 16:58:24 +0200

    fix: remove ws

--- src/view/core/Init.jsx
+++ src/view/core/Init.jsx
@@ -1,28 +1,21 @@
 import React, {useContext, useEffect} from 'react'
 import {ThemeProvider} from '@rneui/themed'
 import {getTheme} from './theme'
-import Router from './Router'
-import Auth from '../components/Auth'
-import Store from './Store'
-import PolyfillCrypto from 'react-native-webview-crypto'
 import useConnection from '../hooks/useConnection'
 import {Context} from './Store'
-import {setConnection, setWeb3, setWs} from '../../data/actions'
+import {setConnection, setWeb3} from '../../data/actions'
 import useWeb3 from '../hooks/useWeb3'
-import useWs from '../hooks/useWs'
 
 const theme = getTheme()
 
 const Init = () => {
-  const [state, dispatch] = useContext(Context)
+  const [, dispatch] = useContext(Context)
   const web3 = useWeb3()
   const connection = useConnection(process.env.CLUSTER_ENDPOINT)
-  const ws = useWs()
   
   useEffect(() => {
     dispatch(setWeb3(web3))
     dispatch(setConnection(connection))
-    dispatch(setWs(ws))
   }, [connection])
 
   return null

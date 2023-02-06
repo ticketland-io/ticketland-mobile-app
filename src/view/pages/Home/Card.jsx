4623bcbef4e614ede873903a4047ec15393b2091
George Niotis <geoniotis@yahoo.com>
2023-02-07 00:41:12 +0200

    feat: adding card component

--- src/view/pages/Home/Card.jsx
+++ src/view/pages/Home/Card.jsx
@@ -1,50 +1,78 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {View} from 'react-native'
-import {Button, Image, Text} from '@rneui/themed'
+import {format} from 'date-fns'
+import {Button, Image, Skeleton, Text} from '@rneui/themed'
 import {Context} from '../../core/Store'
 import Shadow from '../../components/Shadow'
-import useStyles from './styles'
 import UserIcon from '../../../assets/userIcon.png'
 import CalendarIcon from '../../../assets/calendarIcon.png'
-import {get_event_cover_image_path} from '../../../services/event'
+import {fetchAttendedCount, get_event_cover_image_path} from '../../../services/event'
+import useStyles from './styles'
 
 const Card = props => {
+  const {
+    event,
+    style = '',
+    containerStyle = ''
+  } = props
+  const [totalScanned, setTotalScanned] = useState(0)
   const [state] = useContext(Context)
-  const {event, style = ''} = props
   const classes = useStyles()
 
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const {result} = (await fetchAttendedCount(state.firebase, event.event_id))
+        const total = result.reduce((acc, cur) => acc += cur.attended_count, 0)
+
+        setTotalScanned(total)
+      } catch (error) {
+        //ignore
+      }
+    }
+
+    run()
+  }, [event])
+
   return (
-    <View style={{flex: 1}}>
+    <View style={[{flex: 1}, containerStyle]}>
       <Shadow
-        // alignSelf='center'
-        style={[{width: 320, padding: 8}, style]}
+        style={[classes.cardShadow, style]}
         styleInner={{borderRadius: 0}}
       >
         <View justifyContent='center'>
-          <Image source={{uri: get_event_cover_image_path(event?.event_id, event?.file_type)}} style={{width: '100%', height: 212, aspectRatio: 1, borderRadius: 14}} />
+          <Image
+            source={{uri: get_event_cover_image_path(event?.event_id, event?.file_type)}}
+            style={classes.imageCard}
+          />
         </View>
-        <Text h6 style={{marginVertical: 8, paddingHorizontal: 8}}>
-          Music Event Name
+        <Text eventName style={classes.eventName}>
+          {event.name}
         </Text>
-        <View style={{flexDirection: 'row', alignItems: 'center', paddingHorizontal: 8}}>
-          <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+        <View style={classes.infoCardContainer}>
+          <View style={classes.dateItem}>
             <Image
               source={CalendarIcon}
-              style={{width: 14, height: 14, marginRight: 6}}
+              style={classes.calendarImage}
             />
-            <Text h7 style={{fontWeight: 600}}>
-              Date
-            </Text>
+            {event
+              ? (
+                <Text h7 style={{fontWeight: 600}}>
+                  {format(event?.start_date || 0, 'dd.MM.yy')}
+                </Text>
+              )
+              : <Skeleton width={120} height={15}
+              />}
           </View>
-          <View style={{flex: 1, alignItems: 'flex-end'}}>
+          <View style={classes.participantsItem}>
             <Button
-              buttonStyle={{width: 74, height: 34, backgroundColor: 'yellow', padding: 8}}
+              buttonStyle={classes.participantsButton}
             >
               <Image
                 source={UserIcon}
-                style={{width: 14, height: 14, marginRight: 6}}
+                style={classes.participantsImage}
               />
-              <Text h6>2600</Text>
+              <Text h6>{totalScanned}</Text>
             </Button>
           </View>
         </View>

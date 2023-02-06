d8eb73881c2a763ddfcf8c18e799aea281718d9d
George Niotis <geoniotis@yahoo.com>
2023-02-07 00:41:31 +0200

    fix: adding pagination component

--- src/view/pages/Home/Pagination.jsx
+++ src/view/pages/Home/Pagination.jsx
@@ -1,34 +1,35 @@
-import {Text} from '@rneui/base';
-import {Button} from '@rneui/themed';
+import {Button, Text} from '@rneui/themed';
 import React from 'react';
 import {View} from 'react-native';
-import {DOTS, usePagination} from '../../../helpers/pagination'
 import AntIcon from "react-native-vector-icons/AntDesign";
+import {DOTS, usePagination} from '../../../helpers/pagination'
+import useStyles from './styles'
 
 const Pagination = props => {
   const {
     onPageChange,
     totalCount,
-    siblingCount = 1,
     currentPage,
     pageSize,
-    className
   } = props;
+  const classes = useStyles()
 
   const paginationRange = usePagination({
     currentPage,
     totalCount,
-    siblingCount,
     pageSize
   });
 
-
   const onNext = () => {
-    onPageChange(currentPage + 1);
+    if (currentPage < totalCount / pageSize) {
+      onPageChange(currentPage + 1);
+    }
   };
 
   const onPrevious = () => {
-    onPageChange(currentPage - 1);
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
   };
 
   const onCurrent = page => () => {
@@ -36,55 +37,80 @@ const Pagination = props => {
   };
 
   const renderRange = () => (
-    <>
+    <View style={classes.paginationRangeContainer}>
       {paginationRange.map(pageNumber => (pageNumber === DOTS
         ? (
-          <Button type={'outline'} disabled>
-            <Text>&#8230;</Text>
+          <Button
+            type={'outline'}
+            buttonStyle={classes.paginationButton}
+            disabled
+          >
+            <Text>
+              &#8230;
+            </Text>
           </Button>
         )
         : (
-          <Button key={pageNumber} type={'outline'} onPress={onCurrent(pageNumber)}>
-            <Text h4={pageNumber === currentPage ? true : false}>
+          <Button
+            key={pageNumber}
+            type={'outline'}
+            buttonStyle={[
+              classes.paginationButton,
+              {
+                backgroundColor: `${pageNumber === currentPage
+                  ? classes.paginationButton.selected.backgroundColor
+                  : classes.paginationButton.unSelected.backgroundColor
+                  }`
+              }
+            ]}
+            onPress={onCurrent(pageNumber)}
+          >
+            <Text
+              pagination
+              style={{
+                color: `${pageNumber === currentPage
+                    ? classes.paginationButtonText.selected.color
+                    : classes.paginationButtonText.unSelected.color
+                  }`
+              }}
+            >
               {pageNumber}
             </Text>
           </Button>
         )
       ))}
-    </>
+    </View>
   )
 
-  // If there are less than 2 times in pagination range we shall not render the component
-  // if (currentPage === 0 || paginationRange.length < 2) {
-  //   return null;
-  // }
-
-  return currentPage === 0 || paginationRange.length < 2
-    ? null
-    : (
-      <View style={{flexDirection: 'row'}}>
-        <View>
-          <Button
-            type={'outline'}
-            onPress={onPrevious}
-          >
-            <AntIcon
-              name="left"
-              size={15}
-            />
-          </Button>
-        </View>
-        {renderRange()}
-        <View>
-          <Button type={'outline'} onPress={onNext}>
-            <AntIcon
-              name="right"
-              size={15}
-            />
-          </Button>
-        </View>
+  return paginationRange.length > 1 && (
+    <View style={classes.paginationContainer}>
+      <View style={classes.paginationLeftArrow}>
+        <Button
+          type={'outline'}
+          onPress={onPrevious}
+          buttonStyle={classes.paginationButton}
+        >
+          <AntIcon
+            name="left"
+            size={10}
+          />
+        </Button>
       </View>
-    )
+      {renderRange()}
+      <View style={classes.paginationRightArrow}>
+        <Button
+          type={'outline'}
+          onPress={onNext}
+          buttonStyle={classes.paginationButton}
+        >
+          <AntIcon
+            name="right"
+            size={10}
+          />
+        </Button>
+      </View>
+    </View>
+  )
 };
 
 export default Pagination;

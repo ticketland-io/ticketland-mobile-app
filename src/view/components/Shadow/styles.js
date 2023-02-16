63205bcb393780e77a091ce4779ba0cc29439045
George Niotis <geoniotis@yahoo.com>
2023-02-16 11:44:44 +0200

    fix: shadow component style fix for android use

--- src/view/components/Shadow/styles.js
+++ src/view/components/Shadow/styles.js
@@ -3,7 +3,7 @@ import {makeStyles} from '@rneui/themed'
 export default makeStyles(theme => ({
   root: {
     borderWidth: 1,
-    shadowColor: theme.palette.common.black,
+    shadowColor: theme.palette.common.shadow,
     shadowOffset: {width: 5, height: 10},
     shadowOpacity: 1,
     borderRadius: 16,
@@ -13,5 +13,9 @@ export default makeStyles(theme => ({
   inner: {
     borderRadius: 16,
     overflow: 'hidden'
+  },
+  androidShadow: {
+    alignSelf: 'stretch',
+    shadowColor: theme.palette.common.shadow,
   }
 }))

3c30768bfa0ad59ca52471237200b9f458468014
George Niotis <geoniotis@yahoo.com>
2023-03-23 20:56:15 +0200

    feat: adding Event component

---
+++ src/view/pages/Event/index.jsx
@@ -0,0 +1,208 @@
+import React, {useContext, useEffect, useState} from 'react'
+import {Button, Image, Skeleton, Text} from '@rneui/themed'
+import {SafeAreaView, StatusBar, View} from 'react-native'
+import AntIcon from "react-native-vector-icons/AntDesign";
+import {format} from 'date-fns'
+import {
+  fetchAttendedCount,
+  fetchEvent,
+  get_event_cover_image_path,
+  get_event_ticket_image_path
+} from '../../../services/event'
+import {getImageColors, invertColor} from '../../../helpers/color'
+import {Context} from '../../core/Store'
+import CalendarIcon from '../../../assets/calendarIcon.png'
+import QrIcon from '../../../assets/qr-code-line.png'
+import Scanner from '../Scanner'
+import ScannedTickets from './ScannedTickets'
+import useStyles from './styles'
+
+const Event = ({route, navigation}) => {
+  const [state] = useContext(Context)
+  const [event, setEvent] = useState({})
+  const [eventImage, setEventImage] = useState()
+  const [ticketImage, setTicketImage] = useState()
+  const [cameraModalVisible, setCameraModalVisible] = useState(false)
+  const [fullScannedTypes, setFullScannedTypes] = useState([])
+  const [eventFullScanned, setEventFullScanned] = useState(false)
+  const [ticketsCount, setTicketsCount] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [colorFont, setColorFont] = useState(false)
+  const {eventId} = route.params
+  const classes = useStyles()
+
+  useEffect(() => {
+    const run = async () => {
+      setLoading(true)
+
+      try {
+        const [result] = (await fetchEvent(state.firebase, eventId)).result
+        let ticketCounts = (await fetchAttendedCount(state.firebase, eventId)).result
+        const fullScannedTmp = []
+
+        ticketCounts = result.sales.map((sale) => {
+          ticketCounts[sale.ticket_type_index].name = sale.ticket_type_name
+
+          if (ticketCounts[sale.ticket_type_index].attended_count === ticketCounts[sale.ticket_type_index].total_count) {
+            fullScannedTmp.push(sale.ticket_type_name)
+          }
+
+          return ticketCounts[sale.ticket_type_index]
+        })
+
+        const imageColors = await getImageColors(get_event_cover_image_path(result.event_id, result.file_type))
+
+        setColorFont(invertColor(imageColors, true))
+        setFullScannedTypes(fullScannedTmp)
+        setEvent(result)
+        setTicketsCount(ticketCounts)
+        setEventImage(get_event_cover_image_path(result.event_id, result.file_type))
+        setTicketImage(get_event_ticket_image_path(result.event_id, result.file_type))
+
+      } catch (error) {
+        console.log(error)
+        //ignore
+      }
+
+      setLoading(false)
+    }
+
+    run()
+  }, [eventId])
+
+  useEffect(() => {
+    const run = () => {
+      if (fullScannedTypes.length === ticketsCount.length) {
+        setEventFullScanned(true)
+      }
+    }
+
+    fullScannedTypes.length && run()
+  }, [fullScannedTypes])
+
+  const renderHeader = () => (
+    <View style={classes.firstInnerContainer}>
+      <View style={{flex: 2}}>
+        <Button
+          onPress={navigation.goBack}
+          buttonStyle={classes.backButton}
+        >
+          <AntIcon
+            name="left"
+            size={15}
+            style={classes.leftButtonIcon}
+          />
+        </Button>
+      </View>
+      <Text h4 style={[classes.eventName, {color: colorFont}]} >
+        {event.name}
+      </Text>
+      <View style={{flex: 2}} />
+    </View>
+  )
+
+  const renderEvent = () => (
+    <View style={classes.secondInnerContainer}>
+      {!loading
+        ? <Image source={{uri: ticketImage}} containerStyle={classes.ticketImage} />
+        : <Skeleton style={classes.ticketImage} />
+      }
+      <View style={classes.dateContainer}>
+        <View style={classes.dateItem}>
+          <Image
+            source={CalendarIcon}
+            style={classes.calendarIcon}
+          />
+          {!loading
+            ? (
+              <>
+                <Text h7 style={{fontWeight: 500}}>
+                  {format(event?.start_date || 0, 'dd.MM.yy, ')}
+                </Text>
+                <Text h7 style={{fontWeight: 600}}>
+                  {format(event?.start_date || 0, 'HH:mm')}
+                </Text>
+              </>
+            )
+            : <Skeleton style={classes.eventDateSkeleton} />
+          }
+        </View>
+        <View style={classes.dateLine} />
+        <View style={classes.dateItem}>
+          {!loading
+            ? (
+              <>
+                <Text h7 style={{fontWeight: 500}}>
+                  {format(event?.end_date || 0, 'dd.MM.yy, ')}
+                </Text>
+                <Text h7 style={{fontWeight: 600}}>
+                  {format(event?.end_date || 0, 'HH:mm')}
+                </Text>
+              </>
+            )
+            : <Skeleton style={classes.eventDateSkeleton} />
+          }
+        </View>
+      </View>
+    </View>
+  )
+
+  const renderBgImage = () => !loading
+    ? (
+      <View style={classes.eventBgImageContainer}>
+        <Image
+          source={{uri: eventImage}}
+          style={classes.eventBgImage}
+        />
+      </View>
+    )
+    : (
+      <Skeleton
+        height={180}
+        style={classes.eventBgImageSkeleton}
+      />
+    )
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      {
+        Platform.OS === 'ios' &&
+        <StatusBar
+          animated={true}
+          barStyle={colorFont === '#FFFFFF' ? 'light-content' : 'dark-content'}
+        />
+      }
+      {renderBgImage()}
+      <View style={classes.container}>
+        {renderHeader()}
+        {renderEvent()}
+        <ScannedTickets ticketsCount={ticketsCount} loading={loading} />
+        <View style={classes.fourthInnerContainer}>
+          <Scanner
+            modalVisible={cameraModalVisible}
+            setModalVisible={setCameraModalVisible}
+            eventId={eventId}
+            setTicketsCount={setTicketsCount}
+            ticketsCount={ticketsCount}
+          />
+          <Button
+            disabled={eventFullScanned}
+            buttonStyle={classes.scanButton}
+            onPress={() => setCameraModalVisible(true)}
+            loading={false}
+          >
+            {!eventFullScanned && <Image
+              source={QrIcon}
+              style={classes.qrIcon}
+            />}
+            <Text h7 style={!eventFullScanned && classes.scanText}>
+              {eventFullScanned ? 'All tickets have been scanned' : 'Scan Tickets'}
+            </Text>
+          </Button>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Event
---
+++ src/view/pages/Event/styles.js
@@ -0,0 +1,161 @@
+import {makeStyles} from '@rneui/themed'
+
+export default makeStyles(theme => ({
+  container: {
+    flex: 1,
+    padding: 16
+  },
+  firstInnerContainer: {
+    flexDirection: 'row',
+    marginBottom: 38
+  },
+  secondInnerContainer: {
+    marginBottom: 24
+  },
+  scannedTicketsContainer: {
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: theme.palette.common.gray100,
+    backgroundColor: theme.palette.common.gray96,
+    paddingHorizontal: 16,
+    paddingVertical: 8
+  },
+  scannedTicketsInnerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    marginTop: 16
+  },
+  fourthInnerContainer: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  backButton: {
+    width: 36,
+    height: 36,
+    backgroundColor: theme.palette.common.white
+  },
+  leftButtonIcon: {
+    width: 15,
+    height: 15
+  },
+  eventName: {
+    flex: 10,
+    fontWeight: 600,
+    textAlign: 'center',
+  },
+  ticketImage: {
+    borderRadius: 8,
+    height: 114
+  },
+  dateContainer: {
+    marginTop: 8,
+    padding: 7,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: theme.palette.common.gray100,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  dateItem: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  calendarIcon: {
+    width: 14,
+    height: 14,
+    marginRight: 17
+  },
+  dateLine: {
+    height: 1,
+    width: 9,
+    backgroundColor: 'black'
+  },
+  qrIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 8,
+  },
+  ticketIcon: {
+    width: 14,
+    height: 14,
+    marginRight: 9,
+  },
+  checkBoxIcon: {
+    width: 14,
+    height: 14,
+    marginRight: 3,
+  },
+  scanButton: {
+    backgroundColor: theme.palette.common.gray800,
+  },
+  scanText: {
+    color: theme.palette.common.white,
+    paddingVertical: 2
+  },
+  totalVisitorsItem: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  progressBarContainer: {
+    flexDirection: 'row',
+    borderRadius: 8,
+    overflow: 'hidden',
+    position: 'relative',
+    marginTop: 8
+  },
+  progressBarSkeletonContainer: {
+    borderRadius: 8,
+    marginTop: 8,
+    width: '100%',
+    height: 40
+  },
+  yellowProgress: {
+    backgroundColor: theme.palette.common.yellow500,
+    height: 40,
+  },
+  grayProgress: {
+    backgroundColor: theme.palette.common.gray100,
+    height: 40,
+  },
+  progressBarInfoContainer: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 16
+  },
+  progressBarTicketInfoItem: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  successItem: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  eventDateSkeleton: {
+    width: 100,
+    marginVertical: 4
+  },
+  eventBgImageContainer: {
+    position: 'absolute',
+    top: 0,
+    width: '100%'
+  },
+  eventBgImage: {
+    height: 180,
+    borderBottomRightRadius: 24,
+    borderBottomLeftRadius: 24
+  },
+  eventBgImageSkeleton: {
+    position: 'absolute',
+    top: 0,
+    borderBottomRightRadius: 24,
+    borderBottomLeftRadius: 24,
+    height: 180
+  }
+}))

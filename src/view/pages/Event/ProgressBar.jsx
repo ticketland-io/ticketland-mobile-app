fdefe81797fe41666444a69a66aaf2cd0299364c
George Niotis <61066354+GeorgeNiotis@users.noreply.github.com>
2023-03-27 09:54:34 +0300

    Update src/view/pages/Event/ProgressBar.jsx
    Co-authored-by: Antonis Poulakis <30875363+antonispoulakis@users.noreply.github.com>

--- src/view/pages/Event/ProgressBar.jsx
+++ src/view/pages/Event/ProgressBar.jsx
@@ -44,7 +44,6 @@ const ProgressBar = props => {
                       {scannedTickets}
                     </Text>
                   </View>
-
                 )
                 : (
                   <Text>

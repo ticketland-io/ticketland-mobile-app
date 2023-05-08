85503df4dc569bbdd078469cdf4e0c4e2e02d1b2
George Niotis <geoniotis@yahoo.com>
2023-05-08 20:38:27 +0300

    feat: share helper

---
+++ src/helpers/share.js
@@ -0,0 +1,27 @@
+import ReactNativeBlobUtil from 'react-native-blob-util'
+import Share from 'react-native-share'
+import RNFS from 'react-native-fs'
+
+export const sharePdf = (eventId, ticketImage) => {
+  let filePath = null
+
+  const configOptions = {
+    mimeType: 'application/pdf',
+    fileCache: true,
+    path: ReactNativeBlobUtil.fs.dirs.DocumentDir + (`/${eventId}.pdf`)
+  }
+
+  ReactNativeBlobUtil.config(configOptions)
+    .fetch('GET', ticketImage?.url)
+    .then(async resp => {
+      filePath = resp.path()
+      const options = {
+        type: 'application/pdf',
+        url: Platform.OS === 'ios' ? filePath : `file://${filePath}`// (Platform.OS === 'android' ? 'file://' + filePath)
+      }
+
+      await Share.open(options)
+      // remove the pdf from device's storage
+      await RNFS.unlink(filePath)
+    })
+}

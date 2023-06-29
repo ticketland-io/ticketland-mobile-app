4a4592c613a39f45f58de7a1ae2c5866969f0d8b
George Niotis <geoniotis@yahoo.com>
2023-06-29 17:53:47 +0300

    feat: adding helper function fromBase

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,8 +1,3 @@
-import Config from 'react-native-config'
-import * as anchor from '@project-serum/anchor'
-
-const {PublicKey} = anchor.web3
-
 export const qrCodeKeys = [
   'ticketMetadata',
   'ticketNft',
@@ -12,8 +7,3 @@ export const qrCodeKeys = [
   'eventId',
   'expTimestamp',
 ]
-
-export const currencies = {
-  WRAPPED_SOL: new PublicKey('So11111111111111111111111111111111111111112'),
-  USDC: new PublicKey(Config.USDC),
-}

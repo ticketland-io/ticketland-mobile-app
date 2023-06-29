e603a37e8310ce89f466e6c5ad554976c1390fab
George Niotis <geoniotis@yahoo.com>
2023-06-29 17:54:19 +0300

    fix: sign message function

--- src/services/message.js
+++ src/services/message.js
@@ -1,6 +1,5 @@
 import * as borsh from 'borsh'
 import * as bs58 from 'bs58'
-import {Platform} from 'react-native'
 
 class VerifyTicketMsg {
   constructor(eventId, codeChallenge, ticketMetadata) {
@@ -19,17 +18,11 @@ const VerifyTicketMsgType = {
   ]
 }
 
-export const getSignedMessage = async (web3, eventId, codeChallenge, ticketMetadata) => {
+export const getSignedMessage = async (signer, eventId, codeChallenge, ticketMetadata) => {
   const msg = new VerifyTicketMsg(eventId, codeChallenge, ticketMetadata)
-  const schema = new Map([[VerifyTicketMsg, VerifyTicketMsgType]]);
+  const schema = new Map([[VerifyTicketMsg, VerifyTicketMsgType]])
+  const message = borsh.serialize(schema, msg)
+  const {signature} = (await signer.signMessage({message}))
 
-  let message = borsh.serialize(schema, msg)
-
-  if (Platform.OS === 'android') {
-    message = Buffer.from(borsh.serialize(schema, msg))
-  }
-
-  const sig = await web3.anchorProvider.wallet.signMessage(message)
-
-  return bs58.encode(sig)
+  return bs58.encode(Buffer.from(signature))
 }

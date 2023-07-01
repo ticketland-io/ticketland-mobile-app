f1716090237cd637ef2d67b9e002b876c15ddb8a
Antonis Poulakis <antwnic4@gmail.com>
2023-07-02 01:29:12 +0300

    fix: TextEncoder error

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  */
 
 import 'node-libs-react-native/globals.js'
+import 'fast-text-encoding'
 import {AppRegistry} from 'react-native'
 import App from './src/view'
 import {name as appName} from './app.json'
